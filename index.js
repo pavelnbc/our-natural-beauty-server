e0c9a839ebaba857692bd1d103ce16edc82c73c9
giperpasha <giperpasha1992@gmail.com>
2017-11-19 13:19:37 +0200

    Updates for 19.11

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ app.use(cors());
 
 const access = [
     'https://our-natural-beauty.herokuapp.com',
-    // 'https://www.our-natural-beauty.herokuapp.com',
+    'https://www.our-natural-beauty.herokuapp.com',
     'http://our-natural-beauty.herokuapp.com',
-    // 'http://www.our-natural-beauty.herokuapp.com',
+    'http://www.our-natural-beauty.herokuapp.com',
     'http://localhost:3000'
 ];
 
