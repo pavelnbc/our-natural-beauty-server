682e2ad94d8761d577fef97285532bf13ad4617c
pavelnbc <giperpasha@rambler.ru>
2017-12-31 23:36:45 +0200

    Updates for 31.12

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const homePageSlides = require('./api/homePageSlides');
 const menuLinks = require('./api/menuLinks');
 let productCart = [];
 let totalPrice = 0;
+let id = 0;
 
 const app = express();
 
@@ -77,7 +78,7 @@ app.get('/api/v1/cartData', (req, res) => {
 
 app.post('/api/v1/cartData', (req, res) => {
     let good = req.body.orderItem;
-    good.id = productCart.length;
+    good.id = id++;
 
     productCart.push(good);
     totalPrice += good.price;
